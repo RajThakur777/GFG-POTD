3b2869c62a8a289d83c1a78a680768e870d1885b
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-07 22:26:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
@@ -1,34 +1,6 @@
-//{ Driver Code Starts
-// C++ program to merge k sorted arrays of size n each
-#include <bits/stdc++.h>
-using namespace std;
-
-// A Linked List node
-struct Node {
-    int data;
-    Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-/* Function to print nodes in a given linked list */
-void printList(Node* node) {
-    while (node != NULL) {
-        printf("%d ", node->data);
-        node = node->next;
-    }
-    cout << endl;
-}
-
-
-// } Driver Code Ends
-/*Linked list Node structure
-
-struct Node
-{
+/*
+class Node {
+  public:
     int data;
     Node* next;
 
@@ -36,75 +8,30 @@ struct Node
         data = x;
         next = NULL;
     }
-
 };
 */
 
 class Solution {
-public:
+  public:
     Node* mergeKLists(vector<Node*>& arr) {
-        int n = arr.size();
+        // code here
         priority_queue<int, vector<int>, greater<int>> pq;
-        
-        for (int i = 0; i < n; i++) {
-            Node* curr = arr[i];
-            while (curr != nullptr) {
+        for (auto& it : arr){
+            Node* curr = it;
+            while (curr){
                 pq.push(curr->data);
                 curr = curr->next;
             }
         }
-        
-        Node* head = new Node(-1);
-        Node* prev = head;
-        
-        while (!pq.empty()) {
-            Node* newNode = new Node(pq.top());
+        Node* head = new Node(-1000);
+        Node* curr = head;
+        while (!pq.empty()){
+            int val = pq.top();
             pq.pop();
-            prev->next = newNode;
-            prev = newNode;
+            Node* node = new Node(val);
+            curr->next = node;
+            curr = node;
         }
-        
         return head->next;
     }
 };
-
-
-//{ Driver Code Starts.
-
-// Driver program to test the above functions
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n;
-        cin >> n;
-        cin.ignore();
-
-        vector<Node*> v(n);
-
-        for (int i = 0; i < n; i++) {
-            string line;
-            getline(cin, line);
-            stringstream ss(line);
-
-            Node* head = new Node(0);
-            Node* temp = head;
-            int x;
-            while (ss >> x) {
-                Node* newNode = new Node(x);
-                temp->next = newNode;
-                temp = temp->next;
-            }
-            v[i] = head->next;
-        }
-
-        Solution ob;
-        Node* head = ob.mergeKLists(v);
-        printList(head);
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends

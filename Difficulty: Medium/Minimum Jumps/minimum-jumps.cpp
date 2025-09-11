d98f64112d43d2c4307c5dcf27d90ca9626d04cd
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-11 22:44:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -1,62 +1,29 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int fun(int i, vector<int> &a, vector<int> &dp){
-        if(i==a.size()-1)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        
-        int res = INT_MAX;
-        for(int j = i+1 ; j<=min(i+a[i], int(a.size()-1));j++){
-            int c = fun(j, a, dp);
-            
-            if(c!=INT_MAX)
-                res = min(res,1+c);
-        }
-        
-        return dp[i] = res;
-    }
-  
     int minJumps(vector<int>& arr) {
         // code here
         int n = arr.size();
-        vector<int> dp(n+1, -1);
         
-        int ans = fun(0, arr, dp);
-        return ans==INT_MAX?-1:ans;
+        if(n<2)
+          return 0;
+        if(arr[0]==0)
+          return -1;
+        
+        int last=0, count=0, maxi=0;
+        
+        
+        for(int i=0;i<n;i++){
+            maxi = max(maxi, i+arr[i]);
+            
+            if(i==last){
+                count++;
+                last = maxi;
+                if(last >= (n-1))
+                  return count;
+            }
+            
+        }
+        return -1;
     }
 };
 
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, i, j;
-        vector<int> arr;
-        string ip;
-        int number;
-        getline(cin, ip);
-        stringstream ss(ip);
-
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        cout << obj.minJumps(arr) << endl << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends

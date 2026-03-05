0afa77eedf88cc049ff1377be2fad66aeb0df778
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-03-05 08:22:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
@@ -6,13 +6,13 @@ class Solution {
         
         map<char , int> mpp;
         
-        int ans = INT_MIN;
-        
         int i = 0;
+        int ans = -1;
+        
         for(int j=0; j<n; j++) {
             mpp[s[j]]++;
             
-            while(mpp.size() > k) {
+            while((mpp.size() > k)) {
                 mpp[s[i]]--;
                 
                 if(mpp[s[i]] == 0) {
@@ -25,8 +25,6 @@ class Solution {
                 ans = max(ans , (j - i + 1));
             }
         }
-        
-        if(ans == INT_MIN) return -1;
         return ans;
     }
 };

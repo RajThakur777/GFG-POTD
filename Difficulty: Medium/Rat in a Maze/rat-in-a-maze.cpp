813038a095282d275e84f9e15d391d46d7a32af6
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-06-19 07:06:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
@@ -1,37 +1,41 @@
 class Solution {
   public:
+    int m;
     int n;
-    
-    void solve(int i , int j , vector<vector<int>> &maze , string temp , vector<string> &ans) {
-        if(i == n-1 && j == n-1) {
-            ans.push_back(temp);
+
+    vector<string> ans;
+
+    void solve(int i , int j , vector<vector<int> > &grid , int m , int n , string str) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
             return;
         }
         
-        if(i < 0 || i >= n || j < 0 || j >= n || maze[i][j] == 0) return;
-        
-        maze[i][j] = 0;
-        
-        solve(i+1 , j , maze , temp+'D' , ans);
-        solve(i-1 , j , maze , temp+'U' , ans);
-        solve(i , j+1 , maze , temp+'R' , ans);
-        solve(i , j-1 , maze , temp+'L' , ans);
-        
-        maze[i][j] = 1;
+        if(i == m-1 && j == n-1) {
+           ans.push_back(str);
+           return;
+        }
+
+        grid[i][j] = 0;
+
+        solve(i+1 , j , grid , m , n , str + 'D');
+        solve(i-1 , j , grid , m , n , str + 'U');
+        solve(i , j+1 , grid , m , n , str + 'R');
+        solve(i , j-1 , grid , m , n , str + 'L');
+
+        grid[i][j] = 1;
     }
     
     vector<string> ratInMaze(vector<vector<int>>& maze) {
         // code here
+        m = maze.size();
         n = maze[0].size();
-        
-        vector<string> ans;
-        
-        string temp;
-        
-        solve(0 , 0 , maze , temp , ans);
+
+        string str;
+
+        solve(0 , 0 , maze , m , n , str);
         
         sort(ans.begin() , ans.end());
-        
+
         return ans;
     }
 };
